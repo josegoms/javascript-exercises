6a53ab3f33dad27196ad76c583e2b2e658af5031
josegoms <josegomespinheiro26@gmail.com>
2025-03-12 20:40:07 -0300

    Task with reversed arrays and string/array methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let lowerText = text.toLowerCase().replace(/[^\w]|_/g, "");
+    let wordArray = Array.from(lowerText);
 
+    let comparativeArray = wordArray.slice().reverse();
+
+    const originalArray = JSON.stringify(wordArray);
+    const reversedArray = JSON.stringify(comparativeArray);
+
+    if (originalArray === reversedArray) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
