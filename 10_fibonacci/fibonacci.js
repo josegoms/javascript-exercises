8806b6f51d20069e47a9db8e96afa39c741accad
josegoms <josegomespinheiro26@gmail.com>
2025-03-13 12:26:05 -0300

    Task on fibonacci sequence and deal with array indexes

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number == 0) {
+        return 0;
+    }
+    else if (number < 0) {
+        return "OOPS";
+    }
+    let fibonacci = [1, 1];
+    for (let i = 2; i < number; i++) {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
 
+    return fibonacci.at(-1);
 };
 
 // Do not edit below this line
