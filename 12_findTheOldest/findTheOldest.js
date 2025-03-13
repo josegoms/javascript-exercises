4d4b5cfa05ae5ed711d346690563bf4671454eb7
josegoms <josegomespinheiro26@gmail.com>
2025-03-13 15:23:07 -0300

    Task to compare objects inside array, new use to reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const today = new Date();
+        const year = today.getFullYear();
 
+        const oldestAge = (oldest.yearOfDeath || year) - oldest.yearOfBirth;
+        const currentAge = (currentPerson.yearOfDeath || year) - currentPerson.yearOfBirth;
+
+        return (oldestAge > currentAge) ? oldest : currentPerson;
+    });
 };
 
 // Do not edit below this line
